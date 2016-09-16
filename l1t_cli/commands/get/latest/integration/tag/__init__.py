30290d75eb2a15223090073074511a5fb48cf339
kreczko <lkreczko@googlemail.com>
2016-09-16 14:56:17 +0100

    made most method of l1t_cli.commands.get.latest.integration.tag.Command static

--- l1t_cli/commands/get/latest/integration/tag/__init__.py
+++ l1t_cli/commands/get/latest/integration/tag/__init__.py
@@ -26,6 +26,27 @@ class Command(C):
 
     def run(self, args, variables):
         self.__prepare(args, variables)
+
+        versions_and_tags = Command.get_versions_and_tags()
+        latest_tag = Command.get_latest_tag(versions_and_tags)
+
+        self.__text = 'Latest integration tag for {0}: \n'.format(REPO)
+        self.__text += latest_tag
+
+        return True
+
+    @staticmethod
+    def get_versions_and_tags():
+        tags = Command.__get_tags()
+        intagration_tags = filter(lambda x: 'integration' in x, tags)
+        # get versions
+        versions = map(Command.get_version, intagration_tags)
+        versions_and_tags = zip(versions, intagration_tags)
+
+        return versions_and_tags
+
+    @staticmethod
+    def __get_tags():
         str_buffer = StringIO()
         c = pycurl.Curl()
         c.setopt(c.URL, URL)
@@ -35,24 +56,12 @@ class Command(C):
 
         data = json.loads(str_buffer.getvalue())
         tags = [d['name'] for d in data]
+        return tags
 
-        self.__run(tags)
-
-        return True
-
-    def __run(self, tags):
-        intagration_tags = filter(lambda x: 'integration' in x, tags)
-        # get versions
-        versions = map(self.get_version, intagration_tags)
-        versions_and_tags = zip(versions, intagration_tags)
-        latest_tag = self.__get_latest_tag(versions_and_tags)
-
-        self.__text = 'Latest integration tag for {0}: \n'.format(REPO)
-        self.__text += latest_tag
-
-    def __get_latest_tag(self, versions_and_tags):
+    @staticmethod
+    def get_latest_tag(versions_and_tags):
         versions = [v for v, _ in versions_and_tags]
-        latest_version = self.get_latest_version(versions)
+        latest_version = Command.get_latest_version(versions)
         latest_tag = None
         for v, t in versions_and_tags:
             if v == latest_version:
@@ -60,15 +69,16 @@ class Command(C):
                 break
         return latest_tag
 
-    def get_version(self, branch_name):
+    @staticmethod
+    def get_version(branch_name):
         match = re.search('v(\d+\.\d\.?\d?)', branch_name)
         result = match.group()
         result = result.lstrip('v')
         return result
 
-    def get_latest_version(self, versions):
+    @staticmethod
+    def get_latest_version(versions):
         if versions:
             versions.sort(key=lambda s: map(int, s.split('.')), reverse=True)
             return versions[0]
         return None
-
