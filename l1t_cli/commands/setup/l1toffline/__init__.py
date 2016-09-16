9aef4e8681f737bf1c459526124e411531bbfbc8
kreczko <lkreczko@googlemail.com>
2016-09-16 14:57:55 +0100

    added l1toffline setup command

---
+++ l1t_cli/commands/setup/l1toffline/__init__.py
@@ -0,0 +1,51 @@
+"""
+    setup l1toffline:
+        Sets 
+        Usage:
+            setup
+"""
+import logging
+import os
+from hepshell import HEP_PROJECT_ROOT
+from l1t_cli.setup import INTEGRATION_TAG
+
+from .. import Command as C
+
+LOG = logging.getLogger(__name__)
+
+
+class Command(C):
+
+    DEFAULTS = {
+
+    }
+
+    def __init__(self, path=__file__, doc=__doc__):
+        super(Command, self).__init__(path, doc)
+
+    def run(self, args, variables):
+        self.__prepare(args, variables)
+        
+        from l1t_cli.commands.get.latest.integration.tag import Command as Lit
+        versions_and_tags = Lit.get_versions_and_tags()
+        latest_tag = Lit.get_latest_tag(versions_and_tags)
+
+        if latest_tag != INTEGRATION_TAG:
+            LOG.warn('Using integration tag (ITag) {0} but the latest tag is {1}'.format(
+                INTEGRATION_TAG, latest_tag))
+            LOG.warn('If this iTag is not desired, please update setup.json')
+
+        commands = [
+            'git cms-init',
+            'git remote add cms-l1t-offline git@github.com:cms-l1t-offline/cmssw.git',
+            'git fetch cms-l1t-offline',
+            'git cms-merge-topic --unsafe cms-l1t-offline:{0}'.format(
+                INTEGRATION_TAG),
+            'scram b jobs=2',
+        ]
+
+        from l1t_cli.commands.run import run_within_cmssw
+        for command in commands:
+            run_within_cmssw(command, logger=LOG)
+
+        return True
