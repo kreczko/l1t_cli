9c0133f7490b39305da38aed7b7067e073135b31
Lukasz Kreczko <lkreczko@googlemail.com>
2016-09-30 16:09:28 +0100

    added a more general version of cmssw setup

--- l1t_cli/commands/setup/cmssw/__init__.py
+++ l1t_cli/commands/setup/cmssw/__init__.py
@@ -3,16 +3,28 @@
         Sets up the CMS software in the workspace. Workspace must exist.
         
     Usage:
-        setup cmssw version=<cmssw_version>
+        setup cmssw version=<cmssw_version> [alias=<>] [--init-git]\
+                    [parent_folder=<>]
         
     Parameters:
         version: The version of CMSSW to set up (e.g. CMSSW_8_0_0)
                  Default: What is defined in setup.json
 
+        scram_arch: The architecture for the CMSSW version
+                    (e.g. slc6_amd64_gcc530).
+                    Default: What is defined in setup.json
+
+        alias:    Instead of the CMSSW version (CMSSW_X_Y_Z) the project folder
+                  will be set to the value of 'alias'
+
+        parent_folder: Parent folder of the CMSSW project.
+                       Default: $HEP_PROJECT_ROOT/workspace
+
         init-git: Prepares the CMSSW area for merging of git branches.
                   Default:true
 """
 import logging
+import os
 from .. import Command as C
 from hepshell.interpreter import time_function
 
@@ -25,6 +37,9 @@ class Command(C):
     DEFAULTS = {
         'version': CMSSW_VERSION,
         'init-git': True,
+        'parent_folder': WORKSPACE,
+        'alias': '',
+        'scram_arch': SCRAM_ARCH,
     }
 
     def __init__(self, path=__file__, doc=__doc__):
@@ -33,26 +48,37 @@ class Command(C):
     @time_function('setup cmssw', LOG)
     def run(self, args, variables):
         self.__prepare(args, variables)
+
         self.__version = self.__variables['version']
+        self.__scram_arch = self.__variables['scram_arch']
+        self.__alias = self.__variables['alias']
+        if self.__alias == '':
+            self.__alias = self.__version
+        self.__parent_folder = self.__variables['parent_folder']
+        self.__init_git = self.__variables['init-git']
+
         if not self.__can_run():
             return False
 
         commands = [
-            'cd {workspace}',
-            'export SCRAM_ARCH={SCRAM_ARCH}',
+            'cd {parent_folder}',
+            'export SCRAM_ARCH={scram_arch}',
             'source /cvmfs/cms.cern.ch/cmsset_default.sh',
-            '/cvmfs/cms.cern.ch/common/scram project CMSSW {cmssw_version}',
-            'cd {cmssw_version}/src',
-            'eval `/cvmfs/cms.cern.ch/common/scram runtime -sh`',
+            '/cvmfs/cms.cern.ch/common/scram project CMSSW {version} -n {alias}',
         ]
         if self.__variables['init-git']:
-            commands.append('git cms-init')
+            commands.extend([
+                'cd {alias}/src',
+                'eval `/cvmfs/cms.cern.ch/common/scram runtime -sh`',
+                'git cms-init',
+            ])
 
         all_in_one = ' && '.join(commands)
         all_in_one = all_in_one.format(
-            workspace=WORKSPACE,
-            SCRAM_ARCH=SCRAM_ARCH,
-            cmssw_version=self.__version
+            parent_folder=self.__parent_folder,
+            scram_arch=self.__scram_arch,
+            version=self.__version,
+            alias=self.__alias,
         )
 
         from hepshell.interpreter import call
@@ -61,12 +87,12 @@ class Command(C):
         return True
 
     def __can_run(self):
-        import os
-        if not os.path.exists(WORKSPACE):
-            LOG.error('Workspace does not exist: {0}'.format(WORKSPACE))
+        if not os.path.exists(self.__parent_folder):
+            LOG.error(
+                'Parent folder does not exist: {0}'.format(self.__parent_folder))
             return False
-        if os.path.exists(WORKSPACE + '/' + self.__version):
-            LOG.error('CMSSW is already set up: {0}'.format(
-                WORKSPACE + '/' + self.__version))
+        project_path = os.path.join(self.__parent_folder, self.__alias)
+        if os.path.exists(project_path):
+            LOG.error('CMSSW is already set up: {0}'.format(project_path))
             return False
         return True
