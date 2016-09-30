22712c0fbf4fbe40a6c5dd79f9b5e18364d7775a
Lukasz Kreczko <lkreczko@googlemail.com>
2016-09-30 16:17:54 +0100

    added drafts for 2 new commands

---
+++ l1t_cli/commands/setup/dqm/gui/__init__.py
@@ -0,0 +1,28 @@
+"""
+    setup dqm gui:
+        Sets up the DQM GUI. It will be available at port localhost:8060/dqm/dev
+
+        Usage:
+            setup dqm gui
+"""
+import logging
+import os
+import string
+from hepshell.interpreter import time_function
+from l1t_cli.commands import Command as C
+from .. import WORKSPACE
+from l1t_cli.setup import INTEGRATION_TAG
+
+LOG = logging.getLogger(__name__)
+
+
+class Command(C):
+
+    def __init__(self, path=__file__, doc=__doc__):
+        super(Command, self).__init__(path, doc)
+
+    @time_function('setup dqm gui', LOG)
+    def run(self, args, variables):
+        self.__prepare(args, variables)
+            
+        return True
