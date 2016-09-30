11118e8eab0fb1f0d40d710b815a80425d08525c
Lukasz Kreczko <lkreczko@googlemail.com>
2016-09-30 16:08:34 +0100

    renamed "l1t setup offlineDQM" to "l1t setup dpqm offline"

--- l1t_cli/commands/setup/dqm/offline/__init__.py
+++ l1t_cli/commands/setup/dqm/offline/__init__.py
@@ -1,5 +1,7 @@
 """
-    setup offlineDQM: Sets up CMSSW and the latest code to preduce OfflineDQM plots
+    setup dqm offline:
+        Sets up CMSSW and the latest code to preduce OfflineDQM plots
+
         Usage:
             setup offlineDQM
 """
@@ -7,9 +9,8 @@ import logging
 import os
 import string
 from hepshell.interpreter import time_function
-from .. import Command as C
-from .. import WORKSPACE
-from l1t_cli.setup import INTEGRATION_TAG
+from l1t_cli.commands import Command as C
+from l1t_cl.setup import WORKSPACE, INTEGRATION_TAG
 
 LOG = logging.getLogger(__name__)
 
@@ -25,7 +26,7 @@ class Command(C):
     def __init__(self, path=__file__, doc=__doc__):
         super(Command, self).__init__(path, doc)
 
-    @time_function('setup offlineDQM', LOG)
+    @time_function('setup dqm offline', LOG)
     def run(self, args, variables):
         self.__prepare(args, variables)
         self.__version = RECIPE['cmssw_version']
@@ -33,16 +34,24 @@ class Command(C):
         if not os.path.exists(WORKSPACE):
             os.mkdir(WORKSPACE)
 
-        if not self.__can_run():
-            return False
+        from l1t_cli.commands.setup.cmssw import Command as CMSSetup
+        params = {
+            'version': RECIPE['cmssw_version'],
+            'scram_arch': RECIPE['scram_arch'],
+            'alias': 'DQMOffline',
+            'parent_folder': WORKSPACE,
+            'init-git': True,
+        }
+        cmssw = CMSSetup()
+        cmssw.run([], params)
 
         commands = [
-            'git cms-init',
-            'git remote add cms-l1t-offline https://github.com/cms-l1t-offline/cmssw.git',
-            'git fetch cms-l1t-offline',
-            'git cms-merge-topic --unsafe cms-l1t-offline:{0}'.format(RECIPE['l1t_version']),
+            #'git remote add cms-l1t-offline https://github.com/cms-l1t-offline/cmssw.git',
+            #'git fetch cms-l1t-offline',
+            #'git cms-merge-topic --unsafe cms-l1t-offline:{0}'.format(RECIPE['l1t_version']),
             'git cms-addpkg DQMServices/Examples',
             'git cms-addpkg DQMServices/Components',
+            'git ',
             'scram b jobs=2',
         ]
 
@@ -50,7 +59,7 @@ class Command(C):
             'cd {WORKSPACE}',
             'export SCRAM_ARCH={scram_arch}',
             'source /cvmfs/cms.cern.ch/cmsset_default.sh',
-            '/cvmfs/cms.cern.ch/common/scram project CMSSW {cmssw_version}',
+            '/cvmfs/cms.cern.ch/common/scram project CMSSW {cmssw_version} -n offlineDQM',
             'cd {cmssw_version}/src',
             'eval `/cvmfs/cms.cern.ch/common/scram runtime -sh`',
         ]
@@ -61,17 +70,10 @@ class Command(C):
             scram_arch=RECIPE['scram_arch'],
             cmssw_version=RECIPE['cmssw_version'],
         )
-        
+
         from hepshell.interpreter import call
         for command in commands:
             all_in_one = ' {0} && {1}'.format(cmsenv, command)
             call(all_in_one, logger=LOG, shell=True)
-            
-        return True
 
-    def __can_run(self):
-        if os.path.exists(WORKSPACE + '/' + self.__version):
-            LOG.error('CMSSW is already set up: {0}'.format(
-                WORKSPACE + '/' + self.__version))
-            return False
         return True
