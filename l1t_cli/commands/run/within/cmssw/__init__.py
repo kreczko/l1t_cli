7d9145312d68d42c739ce24fed1d9f82b4cafb16
kreczko <lkreczko@googlemail.com>
2016-10-04 15:41:01 +0100

    implemented l1t run within cmssw

--- l1t_cli/commands/run/within/cmssw/__init__.py
+++ l1t_cli/commands/run/within/cmssw/__init__.py
@@ -8,42 +8,53 @@
         Parameters:
             command: the command to run
             
-            cmssw_path: path to the CMSSW setup
+            cmssw_path: path to the CMSSW setup.
 """
 import logging
 import os
 
 from l1t_cli.commands import Command as C
-from l1t_cli.setup import CMSSW_SRC
+from l1t_cli.setup import CMSSW_BASE
 
 LOG = logging.getLogger(__name__)
 
 
 class Command(C):
 
+    DEFAULTS = {
+        'cmssw_path': CMSSW_BASE
+    }
+
     def __init__(self, path=__file__, doc=__doc__):
         super(Command, self).__init__(path, doc)
 
     def run(self, args, variables):
         self.__prepare(args, variables)
-        
-        self.__command == self.__variables['command']
-        self.__text = 'NOT IMPLEMENTED YET'
+        cmssw_path = self.__variables['cmssw_path']
+        if not os.path.exists(cmssw_path):
+            self.__text = "Could not find CMSSW area '{0}'".format(cmssw_path)
+            return False
+        self.__command = args[0]
+
+        commands = [
+            'cd {cmssw_path}/src',
+            'source /cvmfs/cms.cern.ch/cmsset_default.sh',
+            'eval `/cvmfs/cms.cern.ch/common/scram runtime -sh`',
+        ]
+        commands.append(self.__command)
+        all_in_one = ' && '.join(commands)
+        all_in_one = all_in_one.format(cmssw_path=cmssw_path)
+        from hepshell.interpreter import call
+        return_code, stdout, stderr = call(all_in_one, logger=LOG, shell=True)
+        if return_code == 0:
+            self.__text = stdout
+            return True
+        else:
+            self.__text = 'An error occurred when executing\n'
+            self.__text += '"{command}"\n'.format(command=self.__command)
+            self.__text += 'within CMSSW ({cmssw_path}/src):\n'.format(
+                cmssw_path=cmssw_path)
+            self.__text += stderr
+            return False
 
         return True
-
-
-def run_within_cmssw(command, cmssw_src=CMSSW_SRC, logger=LOG):
-    commands = [
-        'cd {CMSSW_SRC}',
-        'source /cvmfs/cms.cern.ch/cmsset_default.sh',
-        'eval `/cvmfs/cms.cern.ch/common/scram runtime -sh`',
-    ]
-
-    all_in_one = ' && '.join(commands)
-    all_in_one = all_in_one + ' && ' + command + ' \n'
-    all_in_one = all_in_one.format(CMSSW_SRC=cmssw_src)
-    from hepshell.interpreter import call
-    return_code, stdout, stderr = call(all_in_one, logger=logger, shell=True)
-
-    return return_code, stdout, stderr
